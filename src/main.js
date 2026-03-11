8dd6a8f40b54e0455969903e109dcc98e0f755c4
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2026-03-11 11:19:06 +0400

    init: add basic folder structure

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const main = async () => {
+  const currentDir = "blabla";
+
+  console.log("Welcome to Data Processing CLI!");
+  console.log(`You are currently in ${currentDir}`);
+};
+
+await main();
